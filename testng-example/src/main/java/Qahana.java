c81e9261b186938fff44f76119df1aa1745552dc
andranik_chorokhyan <andranik_chorokhyan@epam.com>
2021-03-17 12:06:05 +0400

    Add TestNG sample project

---
+++ testng-example/src/main/java/Qahana.java
@@ -0,0 +1,10 @@
+public class Qahana {
+
+    public int aghotel(int amount, String currency) throws Exception {
+        if (amount < 10000) {
+            throw new Exception("Amount is too small");
+        }
+
+        return amount/10;
+    }
+}
